f3cb571d72c99f7efdfd2ba585ab975e1c3e099e
JuttaEm <jutta.ruuskanen@gmail.com>
2021-03-06 19:22:37 +0200

    Frontpage updated, stack navigation added

--- App/components/HomeScreen.js
+++ App/components/HomeScreen.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
+
+
 
 export default function HomeScreen({navigation}) {
+  
+    
   return (
     <View style={styles.container}>
-      <Text>Let's play StashHunters!</Text>
+      <ImageBackground source={require('../assets/kartta.png')} style={styles.image, {width: '100%', height: '100%'}}>
+        
+        <View style={styles.textHeader}>
+          <Text style={{fontSize: 42, fontWeight: 'bold', textAlign: 'center'}} >Let's play StashHunters!</Text>
+        </View>
+        
+        <View style={styles.textInputView}>
+          <TextInput style={styles.textInput} placeholder='username'></TextInput>
+          <TextInput style={styles.textInput} placeholder='password'></TextInput>
+          <Button color='#029B76' title='LOGIN'></Button>
+        </View>
+
+        <View style={styles.textParagraph}>
+          <Text style={{fontSize: 20}}>Create new account</Text>
+          <Text style={{fontSize: 20}} onPress={() => navigation.navigate('BottomNavi')}>Continue without login</Text>
+        </View>
+
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +35,37 @@ export default function HomeScreen({navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff'
+  },
+  image: {
+    flex: 1,
+    opacity: 0.3
+  },
+  textHeader: {
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textParagraph: {
+    color: "black",
+    textAlign: "center",
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white'
   },
+  textInputView: {
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  textInput: {
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 2,
+    width: 200,
+    height: 40,
+    marginBottom: 20,
+    padding: 10
+  }
 });
