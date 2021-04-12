13bf177f2773df54de6538d688848aedf95af91e
miimss <mimmi.rasanen@gmail.com>
2021-04-12 11:23:51 +0300

    fixed currentUser issue

--- App/components/MapScreen.js
+++ App/components/MapScreen.js
@@ -9,13 +9,13 @@ let done = false;
 let lat = 60.201313;
 let long = 24.934041;
 
-export default function MapScreen() {
+export default function MapScreen({ navigation }) {
 
     const [stashes, setStashes] = useState([]);
     //hunted means the stash that the user will try to find
     //this feature is still in progress
     const [hunted, setHunted] = useState([]);
-    const { currentUser } = firebaseAuth;
+    const currentUser = firebaseAuth.currentUser ? firebaseAuth.currentUser : null;
 
     //Haaga-Helia as a preset for mapview to start from something
     const [region, setRegion] = useState({
@@ -46,42 +46,6 @@ export default function MapScreen() {
         }
     }
 
-<<<<<<< HEAD
-  useEffect(() => {
-    getStashes();
-    getUsers();
-  }, []);
-
-  const getUsers = async () => {
-    try {
-      await Firebase.database()
-        .ref('/users')
-        .on('value', snapshot => {
-          const data = snapshot.val();
-          const users = Object.keys(data);
-          let userExists = users.filter(u => u == currentUser.uid);
-
-          if (currentUser.uid !== userExists[0]) {
-            createUserToDatabase();
-          }
-        });
-    } catch (error) {
-      console.log("ALERT! Haussa virhe " + error)
-    }
-
-  }
-
-  const createUserToDatabase = () => {
-    try {
-      Firebase.database().ref('users/' + currentUser.uid).set({ username: currentUser.displayName });
-
-      Alert.alert("User saved");
-    } catch (error) {
-      console.log("Error saving user " + error);
-    }
-  }
-
-=======
     const findLocation = async () => {
 
         let { status } = await Location.requestPermissionsAsync();
@@ -90,7 +54,6 @@ export default function MapScreen() {
                 .then(location => {
                     //setLatitude(location.coords.latitude);
                     //setLongitude(location.coords.longitude);
->>>>>>> d0c98baa1a2b29cdaea96b6075ccb73ebc780c59
 
                     lat = location.coords.latitude;
                     long = location.coords.longitude;
@@ -113,19 +76,61 @@ export default function MapScreen() {
         }
     }
 
+    const getUsers = async () => {
+        if (currentUser) {
+            try {
+                await Firebase.database()
+                    .ref('/users')
+                    .on('value', snapshot => {
+                        const data = snapshot.val();
+                        const users = Object.keys(data);
+                        let userExists = users.filter(u => u == currentUser.uid);
+
+                        if (currentUser.uid !== userExists[0]) {
+                            createUserToDatabase();
+                        }
+                    });
+            } catch (error) {
+                console.log("Error fetching user " + error)
+            }
+        }
+
+
+    }
+
+    const createUserToDatabase = () => {
+        try {
+            Firebase.database().ref('users/' + currentUser.uid).set({ username: currentUser.displayName });
+
+            Alert.alert("User saved");
+        } catch (error) {
+            console.log("Error saving user " + error);
+        }
+    }
 
     useEffect(() => {
         getStashes();
         findLocation();
+        getUsers();
     }, []);
 
 
     return (
         <View style={styles.container}>
             <View style={styles.map}>
-                <View style={styles.header}>
-                    <Text style={styles.headerText}>{currentUser.displayName}</Text>
-                    <Text style={styles.headerText} onPress={handleLogout}>LOGOUT</Text>
+                <View>
+                    {currentUser ?
+                        <View style={styles.header}>
+                            <Text style={styles.headerText}>{currentUser.displayName}</Text>
+                            <Text style={styles.headerText} onPress={handleLogout}>LOGOUT</Text>
+                        </View>
+                        :
+                        <View style={styles.header}>
+                            <Text style={styles.headerText}>anonymous</Text>
+                            <Text style={styles.headerText} onPress={() => navigation.navigate('Home')}>SIGN IN</Text>
+                        </View>
+                    }
+
                 </View>
                 <MapView
                     style={styles.map}
